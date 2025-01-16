f7ee7ff1bc9ada61d0e7736ab40bade4e07a322a
Max Ilyasov <m.ilyasov@dot818.com>
2025-01-16 11:31:49 +0300

    feat: add migration

---
+++ app/migrations/Version20250116082107.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250116082107 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+     $this->addSql(
+                 "CREATE TABLE tblProductData (
+          intProductDataId int(10) unsigned NOT NULL AUTO_INCREMENT,
+          strProductName varchar(50) NOT NULL,
+          strProductDesc varchar(255) NOT NULL,
+          strProductCode varchar(10) NOT NULL,
+          dtmAdded datetime DEFAULT NULL,
+          dtmDiscontinued datetime DEFAULT NULL,
+          stmTimestamp timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+          PRIMARY KEY (intProductDataId),
+          UNIQUE KEY (strProductCode)
+        ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Stores product data';"
+     );
+    }
+
+    public function down(Schema $schema): void
+    {
+
+    }
+}
