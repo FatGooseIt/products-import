ede35c34bed2303767d2a5d287b2349e08ba9acc
daria <daria.g@dot818.com>
2025-01-17 10:32:34 +0300

    feat: prepare environment

---
+++ app/src/Port/Action/IndexAction.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Port\Action;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route(path: '/', name: 'homepage')]
+final class IndexAction
+{
+    public function __invoke(): Response
+    {
+        return new RedirectResponse('/api/doc');
+    }
+}
